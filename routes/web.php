f4dd4349842458ff12479b3d967e8a88683c8c12
Matheus Sturza <2024011863@svs.iffarroupilha.local>
2026-06-03 20:45:18 -0300

    Criando as routes e views

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\AtividadeController;
+use \App\Http\Controllers\CertificadoController;
+use \App\Http\Controllers\EventoController;
+use \App\Http\Controllers\InscricaoController;
+use \App\Http\Controllers\PresencaController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('alunos', AtividadeController::class);
+Route::resource('evento', EventoController::class);
+Route::resource('insricao', InscricaoController::class);
+Route::resource('presenca', PresencaController::class);
+Route::resource('certificado', CertificadoController::class);
+
