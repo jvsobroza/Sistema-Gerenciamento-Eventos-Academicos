1869da96f15cf59a2241993e8b8e47075381ef65
João Victor Sobroza Dal Ross <joaovictor0908.jv@gmail.com>
2026-06-23 19:59:37 -0300

    Adicionando verificador de certificado, junto nas routes

--- routes/web.php
+++ routes/web.php
@@ -66,4 +66,7 @@ Route::get('/certificados/{evento}', [CertificadoController::class, 'evento'])->
 
 Route::get('/certificados/{evento}/{usuario}', [CertificadoController::class, 'pdf'])->name('certificados.pdf');
 
+Route::get('/certificado_verifica', [CertificadoController::class, 'index2'])->name('certificado_verifica');
+Route::get('/verifica_certificado', [CertificadoController::class, 'verifica'])->name('verifica_certificado');
+
 
